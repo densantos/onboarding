a601a5018153c38d8ee174bb1cf26493fc8b828e
Denis Dos Santos <dsantos@Deniss-MacBook-Pro.local>
2023-11-16 08:47:04 -0800

    Initial release

---
+++ .github/workflows/generate_password.py
@@ -0,0 +1,18 @@
+import secrets
+import string
+
+def generate_password(length=12):
+    """Generate a secure random password."""
+    if length < 8:
+        raise ValueError("Password length should be at least 8 characters")
+
+    # Characters to be used in the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generating a random password
+    password = ''.join(secrets.choice(characters) for i in range(length))
+    return password
+
+# Example usage
+password = generate_password(12)
+print(f"Generated Password: {password}")
